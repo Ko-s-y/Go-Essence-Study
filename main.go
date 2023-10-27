942af7ff903663ca234ba22f9202e5ec09f0ca44
s-ko <song.1209@icloud.com>
2023-10-27 19:14:36 +0900

    change: filename

--- main.go
+++ main.go
@@ -5,17 +5,5 @@ import (
 )
 
 func main() {
-	i := 2
-
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Print("two or ")
-		fallthrough // two or three or four が返る
-	case 3, 4:
-		fmt.Println("three or four")
-	default:
-		fmt.Println("other")
-	}
+	fmt.Println("hello, world!")
 }
---
+++ switch_case.go
@@ -0,0 +1,21 @@
+package switch_case
+
+import (
+	"fmt"
+)
+
+func switch_case() {
+	i := 2
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Print("two or ")
+		fallthrough // two or three or four が返る
+	case 3, 4:
+		fmt.Println("three or four")
+	default:
+		fmt.Println("other")
+	}
+}
