9919ce5d96c7e873322408891fa82670a5308e8d
s-ko <song.1209@icloud.com>
2023-11-03 14:24:20 +0900

    interface practice

---
+++ interface.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Speaker interface {
+	Speak() error
+}
+
+type Dog struct{}
+
+func (d *Dog) Speak() error {
+	fmt.Println("BowWow")
+	return nil
+}
+
+type Cat struct{}
+
+func (c *Cat) Speak() error {
+	fmt.Println("Meow")
+	return nil
+}
+
+func DoSpeak(s Speaker) error {
+	return s.Speak()
+}
+
+func main() {
+	dog := Dog{}
+	DoSpeak(&dog)
+	cat := Cat{}
+	DoSpeak(&cat)
+}
